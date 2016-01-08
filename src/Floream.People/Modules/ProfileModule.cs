bde991c45628fbe344236381a01f08170b01d5cb
Mauro Schaparini <Mauro.Schaparini@Floream.com>
2016-01-08 12:11:15 +0000

    Profile changes

---
+++ src/Floream.People/Modules/ProfileModule.cs
@@ -0,0 +1,19 @@
+﻿using Floream.People.DataSources.Context;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Floream.People.Modules
+{
+    public class ProfileModule : NancyModule
+    {
+        private PeopleContext _people;
+
+        public ProfileModule(PeopleContext people)
+        {
+            _people = people;
+        }
+    }
+}
