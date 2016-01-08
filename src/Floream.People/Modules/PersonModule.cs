b8e5aee152d5f659be941854d777d6d821a04b99
Adulai Sow <Adulai.Sow@floream.com>
2016-01-08 11:17:43 +0000

    Added a person module

---
+++ src/Floream.People/Modules/PersonModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Floream.People.DataSources.Context;
+using Nancy;
+
+namespace Floream.People.Modules
+{
+    public class PersonModule : NancyModule
+    {
+        private readonly PeopleContext _peopleContext;
+
+        public PersonModule(PeopleContext peopleContext)
+        {
+            _peopleContext = peopleContext;
+
+            Get["person"] = parameters =>
+            {
+                return null;
+            };
+        }
+
+    }
+}
