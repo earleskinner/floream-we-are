aaee6d309b7853d5b14fa92d3693c6056f1c1b69
Mudasser Jamil <Mudasser.Jamil@floream.com>
2016-01-08 16:06:33 +0000

    header and footer js and css

--- src/Floream.People/Content/js/script.js
+++ src/Floream.People/Content/js/script.js
@@ -4,3 +4,33 @@
     //});
     $('#container').mixItUp();
 });
+
+
+//function (){	     
+
+var $nav = $("nav"),
+	orgNavpos = 70, //offset
+	scrollHeight,
+	scrollTop,
+	$window = $(window),
+	$document = $(document);
+
+function scrollNav() {
+    scrollTop = $document.scrollTop();
+    scrollHeight = scrollTop + $window.height();
+
+    if (scrollTop > orgNavpos) {
+        $nav.addClass("small-nav");
+    } else {
+        $nav.removeClass("small-nav");
+    }
+}
+$window.resize(function () {
+    //init();
+    scrollNav();
+});
+$window.scroll(function () {
+    scrollNav();
+});
+$window.trigger("resize");
+//}
